bc7aa88ed48f80fcd4d7266d97dd70801a3db71c
Ahmad waqas <message2ahmad@gmail.com>
2021-01-05 19:17:09 +1000

    Space modifiers

---
+++ HuliPizza-SwiftUI/Common/ListHeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  ListHeaderView.swift
+//  HuliPizza-SwiftUI
+//
+//  Created by Ahmad Waqas on 5/1/21.
+//
+
+import SwiftUI
+
+struct ListHeaderView: View {
+    var title: String
+    var body: some View {
+        HStack {
+            Text(title)
+                .padding(.leading,5)
+            //can add multiple modifiers as [.leading, trailing, .top, .bottom]
+            Spacer()
+        }
+    }
+}
+
+
+struct ListHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListHeaderView(title: "Menu")
+    }
+}
